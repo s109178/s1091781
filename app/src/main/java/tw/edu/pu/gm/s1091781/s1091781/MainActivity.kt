1fb4b4140c93a5dee6715e48b0d7dd35e6375cd6
s1091781 <sdafdjhfsikjhdf@gmail.com>
2022-05-27 17:00:52 +0800

    v.1

---
+++ app/src/main/java/tw/edu/pu/gm/s1091781/s1091781/MainActivity.kt
@@ -0,0 +1,15 @@
+package tw.edu.pu.gm.s1091781.s1091781
+
+import android.content.pm.ActivityInfo
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+
+    }
+}
